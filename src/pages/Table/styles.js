e8f411fd4b261d0b4af9ea8d60678f286da8f658
JonataRamalho <jonataramalho.s@gmail.com>
2020-08-04 11:47:56 -0300

    Inclui a fonte Quicksand nos componentes text

--- src/pages/Table/styles.js
+++ src/pages/Table/styles.js
@@ -17,6 +17,7 @@ export default StyleSheet.create({
         fontSize: 22,
         color: '#000',
         paddingBottom: 8,
+        fontFamily: 'Quicksand_700Bold',
     },
 
     input: {
@@ -26,6 +27,7 @@ export default StyleSheet.create({
         borderRadius: 8,
         elevation: 3,
         paddingLeft: 24,
+        fontFamily:'Quicksand_500Medium',
         fontSize: 18,
         color: '#000',
     },
@@ -47,13 +49,15 @@ export default StyleSheet.create({
         width: 60,
         height: 60,
         borderRadius: 30,
+        alignItems: 'center',
     },
 
     operationText: {
         color: '#FFF',
         fontSize: 40,
-        fontWeight: "bold",
-        textAlign: "center",
+        fontFamily: 'Quicksand_700Bold',
+        marginTop: -3,
+        marginRight: 1,
     },
 
     resultContent: {
@@ -78,6 +82,7 @@ export default StyleSheet.create({
     },
 
     title: {
+        fontFamily: 'Quicksand_700Bold',
         fontSize: 24,
         color: '#000',
     },
@@ -91,6 +96,7 @@ export default StyleSheet.create({
     },
 
     result: {
+        fontFamily:'Quicksand_500Medium',
         fontSize: 16,
         marginBottom: 16,
     }
